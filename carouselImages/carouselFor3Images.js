9e25c26fcec00aede3f808ff2dac4cd431e69648
Akbarboss <74240696+Akbarboss@users.noreply.github.com>
2021-05-11 17:10:44 +0500

    Add files via upload

---
+++ carouselImages/carouselFor3Images.js
@@ -0,0 +1,151 @@
+// carousel for 3 images
+// container__top
+let images = document.getElementsByTagName("div")[0];
+images.getElementsByTagName("img")[0 ].style.width = "150px";
+images.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images.getElementsByTagName("img")[0 ].style.height = "150px";
+let images1 = document.getElementsByTagName("div")[0];
+images1.getElementsByTagName("img")[1 ].style.width = "150px";
+images1.getElementsByTagName("img")[1 ].style.height = "150px";
+let images1s = document.getElementsByTagName("div")[0];
+images1s.getElementsByTagName("img")[2 ].style.width = "150px";
+images1s.getElementsByTagName("img")[2 ].style.height = "150px";
+
+let images2 = document.getElementsByTagName("div2")[0];
+images2.getElementsByTagName("img")[0 ].style.width = "150px";
+images2.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images2.getElementsByTagName("img")[0 ].style.height = "150px";
+let images3 = document.getElementsByTagName("div2")[0];
+images3.getElementsByTagName("img")[1 ].style.width = "150px";
+images3.getElementsByTagName("img")[1 ].style.height = "150px";
+let images3s = document.getElementsByTagName("div2")[0];
+images3s.getElementsByTagName("img")[2 ].style.width = "150px";
+images3s.getElementsByTagName("img")[2 ].style.height = "150px";
+
+
+let carouselBlock = document.getElementsByTagName("body")[0];
+carouselBlock.getElementsByTagName("main")[0].style.height = "190px";
+
+let carousel = document.getElementsByTagName("main")[0];
+carousel.getElementsByTagName("div")[1].style.height = "230px";
+
+
+let arrow = document.getElementsByTagName("a")[0];
+arrow.getElementsByTagName("span")[0].style.paddingTop = "55px";
+let arrow1 = document.getElementsByTagName("a")[1];
+arrow1.getElementsByTagName("span")[0].style.paddingTop = "55px";
+arrow1.getElementsByTagName("span")[0].style.paddingRight = "30px";
+
+//container__left
+
+let images4 = document.getElementsByTagName("div3")[0];
+images4.getElementsByTagName("img")[0 ].style.width = "200px";
+// images3.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images4.getElementsByTagName("img")[0 ].style.height = "135px";
+let images5 = document.getElementsByTagName("div3")[0];
+images5.getElementsByTagName("img")[1 ].style.width = "200px";
+images5.getElementsByTagName("img")[1 ].style.height = "135px";
+let images5s = document.getElementsByTagName("div3")[0];
+images5s.getElementsByTagName("img")[2 ].style.width = "200px";
+images5s.getElementsByTagName("img")[2 ].style.height = "135px";
+
+let images6 = document.getElementsByTagName("div4")[0];
+images6.getElementsByTagName("img")[0 ].style.width = "200px";
+// images6.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images6.getElementsByTagName("img")[0 ].style.height = "135px";
+let images7 = document.getElementsByTagName("div4")[0];
+images7.getElementsByTagName("img")[1 ].style.width = "200px";
+images7.getElementsByTagName("img")[1 ].style.height = "135px";
+let images7s = document.getElementsByTagName("div4")[0];
+images7s.getElementsByTagName("img")[2 ].style.width = "200px";
+images7s.getElementsByTagName("img")[2 ].style.height = "135px";
+
+let carouselBlock1 = document.getElementsByTagName("body")[0];
+carouselBlock1.getElementsByTagName("main")[1].style.width = "215px";
+let carousel1 = document.getElementsByTagName("main")[1];
+carousel1.getElementsByTagName("div")[1].style.width = "215px";
+carousel1.getElementsByTagName("div")[1].style.marginLeft = "-100px";
+
+let arrow2 = document.getElementsByTagName("a3")[0];
+arrow2.getElementsByTagName("span")[0].style.marginLeft = "-150px";
+arrow2.getElementsByTagName("span")[0].style.marginTop = "-25px";
+let arrow3 = document.getElementsByTagName("a3")[1];
+arrow3.getElementsByTagName("span")[0].style.marginTop = "-25px";
+arrow3.getElementsByTagName("span")[0].style.marginLeft = "130px";
+
+
+// container__bottom
+let images8 = document.getElementsByTagName("div5")[0];
+images8.getElementsByTagName("img")[0 ].style.width = "150px";
+images8.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images8.getElementsByTagName("img")[0 ].style.height = "150px";
+let images9 = document.getElementsByTagName("div5")[0];
+images9.getElementsByTagName("img")[1 ].style.width = "150px";
+images9.getElementsByTagName("img")[1 ].style.height = "150px";
+let images9s = document.getElementsByTagName("div5")[0];
+images9s.getElementsByTagName("img")[2 ].style.width = "150px";
+images9s.getElementsByTagName("img")[2 ].style.height = "150px";
+
+let images10 = document.getElementsByTagName("div6")[0];
+images10.getElementsByTagName("img")[0 ].style.width = "150px";
+images10.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images10.getElementsByTagName("img")[0 ].style.height = "150px";
+let images11 = document.getElementsByTagName("div6")[0];
+images11.getElementsByTagName("img")[1 ].style.width = "150px";
+images11.getElementsByTagName("img")[1 ].style.height = "150px";
+let images11s = document.getElementsByTagName("div6")[0];
+images11s.getElementsByTagName("img")[2 ].style.width = "150px";
+images11s.getElementsByTagName("img")[2 ].style.height = "150px";
+
+let carouselBlock2 = document.getElementsByTagName("body")[0];
+carouselBlock2.getElementsByTagName("main")[2].style.height = "230px";
+carouselBlock2.getElementsByTagName("main")[2].style.marginTop = "30px";
+let carousel2 = document.getElementsByTagName("main")[2];
+carousel2.getElementsByTagName("div")[1].style.height = "230px";
+
+let arrow4 = document.getElementsByTagName("a4")[0];
+arrow4.getElementsByTagName("span")[0].style.paddingTop = "45px";
+let arrow5 = document.getElementsByTagName("a4")[1];
+arrow5.getElementsByTagName("span")[0].style.paddingTop = "45px";
+arrow5.getElementsByTagName("span")[0].style.paddingRight = "25px";
+
+//container__right
+
+let images12 = document.getElementsByTagName("div7")[0];
+images12.getElementsByTagName("img")[0 ].style.width = "200px";
+// images3.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images12.getElementsByTagName("img")[0 ].style.height = "135px";
+let images13 = document.getElementsByTagName("div7")[0];
+images13.getElementsByTagName("img")[1 ].style.width = "200px";
+images13.getElementsByTagName("img")[1 ].style.height = "135px";
+let images13s = document.getElementsByTagName("div7")[0];
+images13s.getElementsByTagName("img")[2 ].style.width = "200px";
+images13s.getElementsByTagName("img")[2 ].style.height = "135px";
+
+let images14 = document.getElementsByTagName("div8")[0];
+images14.getElementsByTagName("img")[0 ].style.width = "200px";
+// images6.getElementsByTagName("img")[0 ].style.marginLeft = "15px";
+images14.getElementsByTagName("img")[0 ].style.height = "135px";
+let images15 = document.getElementsByTagName("div8")[0];
+images15.getElementsByTagName("img")[1 ].style.width = "200px";
+images15.getElementsByTagName("img")[1 ].style.height = "135px";
+let images15s = document.getElementsByTagName("div8")[0];
+images15s.getElementsByTagName("img")[2 ].style.width = "200px";
+images15s.getElementsByTagName("img")[2 ].style.height = "135px";
+
+
+let carouselBlock3 = document.getElementsByTagName("body")[0];
+carouselBlock3.getElementsByTagName("main")[3].style.width = "220px";
+carouselBlock3.getElementsByTagName("main")[3].style.widt = "220px";
+let carousel3 = document.getElementsByTagName("main")[3];
+carousel3.getElementsByTagName("div")[1].style.width = "220px";
+carousel3.getElementsByTagName("div")[1].style.marginLeft = "20px";
+
+let arrow6 = document.getElementsByTagName("a5")[0];
+arrow6.getElementsByTagName("span")[0].style.marginLeft = "-20px";
+arrow6.getElementsByTagName("span")[0].style.marginTop = "-25px";
+let arrow7 = document.getElementsByTagName("a5")[1];
+arrow7.getElementsByTagName("span")[0].style.marginTop = "-25px";
+arrow7.getElementsByTagName("span")[0].style.marginLeft = "250px";
+
+// carousel for 3 images ended
